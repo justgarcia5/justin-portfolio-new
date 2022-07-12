68135529d509c2116fc7cabe3a8c0dbc81e65250
JUSTGARCIA5 <JUSTGARCIA5@GMAIL.COM>
2022-07-12 14:56:56 -0700

    updated contact section with psd iphone

--- src/components/Layout/Contact/ContactCol.js
+++ src/components/Layout/Contact/ContactCol.js
@@ -3,10 +3,12 @@ import React from 'react';
 import classes from './ContactCol.module.css';
 import Iphone from '../../../images/iphone.png'
 
+
 const ContactCol = () => {
     return (
         <div className={classes['contact-col']} >
           <img src={Iphone} alt='' />
+          {/* <img className={classes.background} src={Background} alt='' /> */}
         </div>
     )
 };
--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
+import { FaFacebookF } from "react-icons/fa";
+import { FaCodepen } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
+
 import classes from './Footer.module.css';
 
 const Footer = () => {
   return(
     <React.Fragment>
       <footer>
-          <div className="icons d-flex justify-content-center">
-            <a href="https://www.facebook.com/justin.garcia.378199" className='icon-foot p-2' target="_blank" rel="noreferrer"><i className="fa fa-facebook"></i></a>
-            <a href="https://codepen.io/Freshmex" className='icon-foot p-2' target="_blank" rel="noreferrer"><i className="fa fa-codepen"></i></a>
-            <a href="https://github.com/justgarcia5" className='icon-foot p-2' target="_blank" rel="noreferrer"><i className="fa fa-github"></i></a>
-            <a href="https://www.linkedin.com/in/just-g-garcia/" className='icon-foot p-2' target="_blank" rel="noreferrer"><i className="fa fa-linkedin"></i></a>
-            <a href="mailto:justgarcia5@gmail.com" className='icon-foot p-2' target="_blank" rel="noreferrer"><i className="fa fa-envelope"></i></a>
+          <div className={classes.icons}>
+            <a href="https://www.facebook.com/justin.garcia.378199" className='icon-foot p-2' target="_blank" rel="noreferrer"><FaFacebookF /></a>
+            <a href="https://codepen.io/Freshmex" className='icon-foot p-2' target="_blank" rel="noreferrer"><FaCodepen /></a>
+            <a href="https://github.com/justgarcia5" className='icon-foot p-2' target="_blank" rel="noreferrer"><FaGithub /></a>
+            <a href="https://www.linkedin.com/in/just-g-garcia/" className='icon-foot p-2' target="_blank" rel="noreferrer"><FaLinkedin /></a>
+            <a href="mailto:justgarcia5@gmail.com" className='icon-foot p-2' target="_blank" rel="noreferrer"><FaEnvelope /></a>
           </div>
           <br/>
           <p><a href="mailto:justgarcia5@gmail.com">Justin Garcia</a> © 2022 | San Diego, Ca</p>
